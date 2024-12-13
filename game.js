4992fca307be93d29c008368e168f2ed0892c6fb
Keith Parum <23403050+keithparum@users.noreply.github.com>
2024-12-13 22:00:40 +0100

    corrected typo in index.html and added dummy logic in game.js for testing

--- game.js
+++ game.js
@@ -4,3 +4,17 @@ const decoyRock = document.querySelector(".rock-container");
 const decoyPaper = document.querySelector(".paper-container");
 const decoyScissors = document.querySelector(".scissors-container");
 const messageBoard = document.querySelector(".message-container");
+
+decoyRock.addEventListener("click", decoyRockClick);
+decoyPaper.addEventListener("click", decoyPaperClick);
+decoyScissors.addEventListener("click", decoyScissorsClick);
+
+function decoyRockClick() {
+  console.log("You clicked on the ROCK!");
+}
+function decoyPaperClick() {
+  console.log("You clicked on the PAPER!");
+}
+function decoyScissorsClick() {
+  console.log("You clicked on the SCISSORS!");
+}
